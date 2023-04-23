1c2c284e230a816d9d39b25c4b81910ae07ca777
KSungGyu <ksg980105@naver.com>
2023-04-23 15:22:41 +0900

    Initial ommit

---
+++ src/main/java/com/example/account/domain/Transaction.java
@@ -0,0 +1,42 @@
+package com.example.account.domain;
+
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Transaction {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Account account;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+    @Enumerated(EnumType.STRING)
+    private TransactionResultType transactionResultType;
+
+    private Long amount;
+    private Long balanceSnapshot;
+    private String transactionId;
+    private LocalDateTime transactedAt;
+
+    @CreatedDate
+    private LocalDateTime createAt;
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+}
